b3ae8e1bc5aa38fddbfa2a3f893819b4dbc0a171
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-09-19 18:02:41 +0200

    ejercicio 1 hecho

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--Santiago Calderon Castaño-->
+</head>
+
+<body>
+    <h1>Página de prueba</h1>
+    <?php
+    $nombre = "Santiago Calderon";
+    $anyo = 2003;
+    ?>
+    <p>Me llamo <?php echo $nombre; ?> y nací en el año <?php echo $anyo; ?> .</p>
+</body>
+</html>
