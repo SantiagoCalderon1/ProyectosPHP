544257bc5ebac3382e263ea659d638f1e3fd0edd
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-09-19 19:27:50 +0200

    Ejercicio de area del circulo hecha

---
+++ ejercicios1/area_circulo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 02</title>
+    <?php
+    define('PI',  3.14159265358979323846);
+    #const PI = 3.14159265358979323846;
+    ?>
+</head>
+
+<body>
+    <h1>Ejercicio 02</h1>
+
+    <?php
+    $radio = 3.5;
+    $area = PI * pow($radio, 2);
+    echo  "El área del círculo es: $area";
+    ?>
+
+</body>
+
+</html>
