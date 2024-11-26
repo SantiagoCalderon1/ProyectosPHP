b5afc94742926e096cceaf05b5737fab4cce939d
Santiago Calderon <santiago.contactoyt@gmail.com>
2024-11-26 12:05:29 +0100

    Ejercicio 01 de practica hecho

--- PracticaExamen/eje01/Usuario.php
+++ PracticaExamen/eje01/Usuario.php
@@ -1,5 +1,7 @@
 <?php
+include 'Persona.php';
+
 class Usuario extends Persona
 {
-
+    // NO es necesario más codigo, ya que todo lo heredad el padre, el constructor y los métodos.
 }
--- PracticaExamen/eje01/eje01.php
+++ PracticaExamen/eje01/eje01.php
@@ -8,38 +8,75 @@
 </head>
 
 <body>
+    <h1>holaa</h1>
     <?php
+    echo '<h1>Json_encode() y Json_decode()</h1>';
 
-    $usuario1 = [
-        'nombre' => 'Juan Velez',
-        'edad' => 25,
-        'email' => 'abc@def.com'
-    ];
-    $usuario2 = [
-        'nombre' => 'Camila Sepulveda',
-        'edad' => 23,
-        'email' => 'abc@def.com'
-    ];
-    $usuario3 = [
-        'nombre' => 'Danna Mora',
-        'edad' => 19,
-        'email' => 'abc@def.com'
-    ];
+    include 'Usuario.php';
 
     $usuarios = [
-        'usuario1' => $usuario1,
-        'usuario2' => $usuario2,
-        'usuario3' => $usuario3,
+        'user1' => new Usuario('Jorge', 20, 'abc@def.com'),
+        'user2' => new Usuario('Andres', 21, 'abc@def.com'),
+        'user3' => new Usuario('Camilo', 22, 'abc@def.com')
     ];
 
-    
+    function toJson(array $usuarios): string
+    {
+        $usuariosArray = array_map(function ($usuario): array {
+            return [
+                'nombre' => $usuario->getNombre(),
+                'edad' => $usuario->getEdad(),
+                'email' => $usuario->getEmail()
+            ];
+        }, $usuarios);
+        return json_encode($usuariosArray, JSON_PRETTY_PRINT);
+    }
 
+    function writeFile(string $Json): void
+    {
+        file_put_contents('pruebaJson.txt', $Json);
+    }
 
+    writeFile(toJson($usuarios));
+    echo '<p>Array de usuarios</p>';
+    echo toJson($usuarios);
+    echo '<p>Fichero escrito correctamente</p>';
 
+    echo '<p>leyendo fichero</p>';
 
+    // Función para leer un fichero y mostrar su contenido
+    function readfileContent(string $rutaFile): void
+    {
+        if (file_exists($rutaFile)) {
+            echo '<p><strong>Contenido del fichero (decodificado):</strong></p>';
+            $contenido = json_decode(file_get_contents($rutaFile), true); // Convertir a array asociativo
+            //echo '<pre>' . print_r($contenido, true) . '</pre>';
 
+            foreach ($contenido as $key => $usuario) {
+                echo '<tr>
+                        <td>' . $key . '</td>
+                        <td>' . $usuario['nombre'] . '</td>
+                        <td>' . $usuario['edad'] . '</td>
+                        <td>' . $usuario['email'] . '</td>
+                    </tr>';
+            }
+        } else {
+            echo '<p><strong>El fichero no existe</strong></p>';
+        }
+    }
+    //readfile('pruebaJson.txt');
     ?>
 
+    <table>
+        <tr>
+            <th>Usuario</th>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Email</th>
+        </tr>
+        <?php readfileContent('pruebaJson.txt');
+        ?>
+    </table>
 </body>
 
 </html>
