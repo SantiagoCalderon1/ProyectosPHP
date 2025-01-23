877abd298a9875cc60a968f9d5ca663d51f56d45
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2025-01-23 19:24:42 +0100

    a

--- ProyectoTiendaAJAX/app/controllers/controllerClient.php
+++ ProyectoTiendaAJAX/app/controllers/controllerClient.php
@@ -3,15 +3,30 @@ include_once '../models/client.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'] ?? '';
-    switch ($action) {
-        case '':
 
+    switch ($action) {
+        case 'updateClient':
+            $json = file_get_contents("php://input");
+
+            $client = json_decode($json, true);
+
+            if ($client) {
+                $id = $client['id'] ?? '';
+                $nombre = $client['nombre'] ?? '';
+                $apellidos = $client['apellidos'] ?? '';
+            }
+
+            $clientAux = new Client($id, $nombre, $apellidos);
+
+            $clientAux->updateClient();
+            break;
+        case '':
+            break;
         default:
             http_response_code(400);
             echo json_encode(['error' => 'Acción no reconocida.']);
             exit;
     }
-
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -22,13 +37,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $clients = client::getAllData();
 
             // Enviar respuesta en JSON
-            header('Content-Type: application/json');
-            echo json_encode($clients);
+             header('Content-Type: application/json');
+             echo json_encode($clients);
+
             exit;
 
         case 'getClient':
             $client = client::selectClient($id);
-            
+
             header('Content-Type: application/json');
             echo json_encode($client);
             break;
